795493e03aebe7f5c62f509996c78d2c1c0b9a97
krzaq <krzaq@krzaq.cc>
2022-08-30 03:02:25 +0200

    asdasd

--- client/crashpad_client_win.cc
+++ client/crashpad_client_win.cc
@@ -820,8 +820,8 @@ void CrashpadClient::DumpWithoutCrashWithException(EXCEPTION_POINTERS* pointer) 
   //exception_pointers.ContextRecord = const_cast<CONTEXT*>(&context);
 
   //In this case we don't want to mock fake EXCEPTION_POINTER.
-  g_wer_registration.dump_without_crashing.thread_id = GetCurrentThreadId();
-  g_wer_registration.dump_without_crashing.exception_pointers =
+  g_non_crash_exception_information.thread_id = GetCurrentThreadId();
+  g_non_crash_exception_information.exception_pointers =
       FromPointerCast<WinVMAddress>(&exception_pointers);
 
   g_wer_registration.in_dump_without_crashing = true;
